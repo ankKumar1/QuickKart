858739e97bc4ab6b7cd90553b5d301d864e46686
Ankit <iamankitkumar546@gmail.com>
2025-09-20 22:47:10 +0530

    added purchase repo

---
+++ PurchaseService/Controllers/PurchaseServiceController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PurchaseService.Models;
+using PurchaseService.Repository;
+
+namespace PurchaseService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchaseServiceController : Controller
+    {
+        private readonly PurchaseRepository repository;
+
+        public PurchaseServiceController(PurchaseRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        [HttpGet]
+        public JsonResult GetAllPurchasesDetails()
+        {
+            List<Purchase> listOfPurchases = repository.GetAllPurchases();
+            return Json(listOfPurchases);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetPurchaseById(int id)
+        {
+            List<Purchase> listOfPurchases = repository.GetAllPurchases();
+            Purchase purchase = listOfPurchases.Find(p => p.PurchaseId == id);
+            return Json(purchase);
+        }
+
+        [HttpPost]
+        public JsonResult AddNewPurchase(Purchase purchase)
+        {
+            return Json(repository.AddNewPurchase(purchase));
+        }
+
+        [HttpPut]
+        public JsonResult UpdatePurchase(Purchase purchase)
+        {
+            int result = repository.UpdatePurchaseDetails(purchase);
+            return Json(result);
+        }
+
+        [HttpDelete]
+        public JsonResult DeletePurchase(int id)
+        {
+            bool result = repository.DeletePurchase(id);
+            return Json(result);
+        }
+    }
+}
---
+++ PurchaseService/Models/PurchaseDBContext.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PurchaseService.Models
+{
+    public class PurchaseDBContext : DbContext
+    {
+        public PurchaseDBContext()
+        {
+
+        }
+        public PurchaseDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+        public DbSet<Purchase> Purchases { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            var config = builder.Build();
+            var connectionString = config.GetConnectionString("PurchaseDBConnectionString");
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Purchase>().HasData(
+                new Purchase
+                {
+                    PurchaseId = 1,
+                    EmailId = "Franken@gmail.com",
+                    ProductId = "P101",
+                    QuantityPurchased = 2,
+                    TotalPrice = 9000
+                },
+                new Purchase
+                {
+                    PurchaseId = 2,
+                    EmailId = "Franken@gmail.com",
+                    ProductId = "P143",
+                    QuantityPurchased = 1,
+                    TotalPrice = 1114.52M
+                },
+                new Purchase
+                {
+                    PurchaseId = 3,
+                    EmailId = "SamRock@gmail.com",
+                    ProductId = "P120",
+                    QuantityPurchased = 4,
+                    TotalPrice = 2085
+                },
+                new Purchase
+                {
+                    PurchaseId = 4,
+                    EmailId = "PaulGrey@gmail.com",
+                    ProductId = "P148",
+                    QuantityPurchased = 1,
+                    TotalPrice = 2561.51M
+                },
+                new Purchase
+                {
+                    PurchaseId = 5,
+                    EmailId = "PaulGrey@gmail.com",
+                    ProductId = "P101",
+                    QuantityPurchased = 10,
+                    TotalPrice = 1265
+                }
+            );
+
+        }
+
+    }
+}
